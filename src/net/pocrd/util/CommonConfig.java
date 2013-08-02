c605e74ee3a0ff14b6e261593c481fb972f5ef0a
Tim.Guan <gkqwt1990@gmail.com>
2013-08-03 00:14:41 +0800

    CacheProvider 支持基础cache功能

--- src/net/pocrd/util/CommonConfig.java
+++ src/net/pocrd/util/CommonConfig.java
@@ -8,36 +8,50 @@ import org.apache.logging.log4j.Logger;
 
 @XmlRootElement
 public class CommonConfig {
-    public static final CommonConfig Instance;
+	public static final CommonConfig Instance;
 
-    public static final boolean      isDebug = true;
+	public static final boolean isDebug = true;
 
-    private CommonConfig() {}
+	private CommonConfig() {
+	}
 
-    static {
-        CommonConfig tmp = ConfigUtil.load("Common.config", CommonConfig.class);
-        // 默认值设置
-        if (tmp == null) {
-            tmp = new CommonConfig();
-            tmp.autogenPath = "E:\\software\\jd\\";
-            tmp.tokenPwd = "KaKS8hro1Ljf7YXIK+iiag5ofiPmaucUqqfBTu7eVVI=";
-            tmp.accessLoggerName = "net.pocrd.api.access";
-            tmp.staticSignPwd = "pocrd@gmail.com";
-        }
-        Instance = tmp;
-        Instance.accessLogger = LogManager.getLogger(Instance.accessLoggerName);
-        Instance.tokenHelper = new TokenHelper(Instance.tokenPwd);
-    }
+	static {
+		CommonConfig tmp = ConfigUtil.load("Common.config", CommonConfig.class);
+		// 默认值设置
+		if (tmp == null) {
+			tmp = new CommonConfig();
+			tmp.autogenPath = "E:\\software\\jd\\";
+			tmp.tokenPwd = "KaKS8hro1Ljf7YXIK+iiag5ofiPmaucUqqfBTu7eVVI=";
+			tmp.accessLoggerName = "net.pocrd.api.access";
+			tmp.staticSignPwd = "pocrd@gmail.com";
+			tmp.cacheVersion = "v1.0";
+			tmp.cacheType = CacheDBType.Redis;
+		}
+		Instance = tmp;
+		Instance.accessLogger = LogManager.getLogger(Instance.accessLoggerName);
+		Instance.tokenHelper = new TokenHelper(Instance.tokenPwd);
+	}
 
-    public String      accessLoggerName;
-    public String      tokenPwd;
-    public String      staticSignPwd;
-    public String      autogenPath;
+	public String accessLoggerName;
+	public String tokenPwd;
+	public String staticSignPwd;
+	public String autogenPath;
+	public String cacheVersion;
+	public CacheDBType cacheType;
 
-    @XmlTransient
-    public TokenHelper tokenHelper;
+	@XmlTransient
+	public TokenHelper tokenHelper;
 
-    @XmlTransient
-    public Logger      accessLogger;
+	@XmlTransient
+	public Logger accessLogger;
 
+	/**
+	 * 缓存实现机制
+	 * 
+	 * @author guankaiqiang
+	 * 
+	 */
+	public enum CacheDBType {
+		Redis,Memcache
+	}
 }
