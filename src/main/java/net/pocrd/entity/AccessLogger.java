faa6cfe2137c41cbeb4bf01e9b1818ff08349aea
rendong <leorendong@hotmail.com>
2016-08-26 17:18:16 +0800

    [dev] do refactory

--- src/main/java/net/pocrd/entity/AccessLogger.java
+++ src/main/java/net/pocrd/entity/AccessLogger.java
@@ -1,5 +1,6 @@
 package net.pocrd.entity;
 
+import net.pocrd.core.HttpRequestExecuter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,7 +24,7 @@ public final class AccessLogger {
      * no error
      */
     public void logRequest() {
-        ApiContext apiContext = ApiContext.getCurrent();
+        ApiContext apiContext = HttpRequestExecuter.get().getApiContext();
         requestFileLogger.info(
                 apiContext.getRequestString().replace("\n", "") + ACCESS_SPLITTER + apiContext.agent + ACCESS_SPLITTER + apiContext.clientIP
                         + ACCESS_SPLITTER
@@ -39,7 +40,7 @@ public final class AccessLogger {
      * @param data
      */
     public void logRequest(String errorMsg, String data) {
-        ApiContext apiContext = ApiContext.getCurrent();
+        ApiContext apiContext = HttpRequestExecuter.get().getApiContext();
         requestFileLogger.info(
                 apiContext.getRequestString().replace("\n", "") + ACCESS_SPLITTER + apiContext.agent + ACCESS_SPLITTER + apiContext.clientIP
                         + ACCESS_SPLITTER + apiContext.token + ACCESS_SPLITTER + errorMsg + ACCESS_SPLITTER + data + ACCESS_SPLITTER
@@ -59,7 +60,7 @@ public final class AccessLogger {
      */
     public void logAccess(int costTime, String methodName, int returnCode, int orginReturnCode, int resultLen, String callMsg,
             String serviceLog) {
-        ApiContext apiContext = ApiContext.getCurrent();
+        ApiContext apiContext = HttpRequestExecuter.get().getApiContext();
         accessFileLogger.info(costTime + ACCESS_SPLITTER + methodName + ACCESS_SPLITTER + returnCode + ACCESS_SPLITTER + orginReturnCode
                 + ACCESS_SPLITTER + resultLen + ACCESS_SPLITTER + (callMsg == null ? "" : callMsg.replace("\n", "")) + ACCESS_SPLITTER +
                 apiContext.startTime + ":" + serviceLog);
